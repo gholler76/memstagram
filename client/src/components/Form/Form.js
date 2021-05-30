996b06e102ffb4d34c073f39fd8b479b17ebc4b3
Gary Holler <gholler76@gmail.com>
2021-05-30 01:09:52 -0400

    fixed the form clear function

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -29,11 +29,19 @@ const Form = ({ currentId, setCurrentId }) => {
         } else {
             dispatch(createPost(postData));
         }
+        clear();
     }
 
 
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({
+            creator: "",
+            title: "",
+            message: "",
+            tags: "",
+            selectedFile: ""
+        });
     }
 
     return (
