248acec2d4233a2a925be0b12c85e9cbe4d4e932
Gary Holler <gholler76@gmail.com>
2021-05-29 23:43:46 -0400

    implemented basic like functionality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,7 @@
 // set all routes having to do with posts
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 // set up express as the router
 const router = express.Router();
@@ -11,5 +11,6 @@ router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
 router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
