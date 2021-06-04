6f48717bf9d2540f8aee69dd496434a988851429
Gary Holler <gholler76@gmail.com>
2021-06-04 14:30:58 -0400

    added static pagination component to home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,33 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { useDispatch } from 'react-redux';
-import { getPosts } from '../../actions/posts'
+import { getPosts } from '../../actions/posts';
+import Pagination from '../Pagination';
 
 const Home = () => {
-    const [currentId, setCurrentId] = useState(0);
+    const [ currentId, setCurrentId ] = useState( 0 );
     const dispatch = useDispatch();
 
     // used to dispatch actions
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch])
+    useEffect( () => {
+        dispatch( getPosts() );
+    }, [ currentId, dispatch ] );
 
     return (
         <Grow in>
             <Container>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts setCurrentId={setCurrentId} />
+                <Grid container justify="space-between" alignItems="stretch" spacing={ 3 }>
+                    <Grid item xs={ 12 } sm={ 7 }>
+                        <Posts setCurrentId={ setCurrentId } />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                    <Grid item xs={ 12 } sm={ 4 }>
+                        <Form currentId={ currentId } setCurrentId={ setCurrentId } />
+                        <Paper elevation={ 6 }>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
-        </Grow>
-    )
+        </Grow >
+    );
 };
 
 export default Home;
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -5,53 +5,64 @@ import memories from '../../images/memories.png';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { LOGOUT } from '../../constants/actionTypes';
+import decode from 'jwt-decode';
 
 
 const Navbar = () => {
     const classes = useStyles();
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const [ user, setUser ] = useState( JSON.parse( localStorage.getItem( 'profile' ) ) );
     const dispatch = useDispatch();
     const history = useHistory();
     const location = useLocation();
 
     const logout = () => {
-        dispatch({ type: LOGOUT });
+        dispatch( { type: LOGOUT } );
 
-        history.push('/');
+        history.push( '/' );
 
-        setUser(null);
+        setUser( null );
     };
 
-    useEffect(() => {
+    useEffect( () => {
         const token = user?.token;
 
-        setUser(JSON.parse(localStorage.getItem('profile')));
-    }, [location])
+        if ( token )
+        {
+            const decodedToken = decode( token );
+
+            if ( decodedToken.exp * 1000 < new Date().getTime() ) logout();
+        }
+
+
+
+
+        setUser( JSON.parse( localStorage.getItem( 'profile' ) ) );
+    }, [ location ] );
 
 
 
 
     return (
-        <AppBar className={classes.appBar} position="static" color="inherit">
-            <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memstagram</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
+        <AppBar className={ classes.appBar } position="static" color="inherit">
+            <div className={ classes.brandContainer }>
+                <Typography component={ Link } to="/" className={ classes.heading } variant="h2" align="center">Memstagram</Typography>
+                <img className={ classes.image } src={ memories } alt="memories" height="60" />
             </div>
-            <Toolbar className={classes.toolbar}>
-                {user ? (
-                    <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Toolbar className={ classes.toolbar }>
+                { user ? (
+                    <div className={ classes.profile }>
+                        <Avatar className={ classes.purple } alt={ user.result.name } src={ user.result.imageUrl }>{ user.result.name.charAt( 0 ) }</Avatar>
+                        <Typography className={ classes.userName } variant="h6">{ user.result.name }</Typography>
+                        <Button variant="contained" className={ classes.logout } color="secondary" onClick={ logout }>Logout</Button>
                     </div>
                 ) : (
-                    <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                    <Button component={ Link } to="/auth" variant="contained" color="primary">Sign In</Button>
                 )
                 }
             </Toolbar>
         </AppBar >
 
-    )
+    );
 };
 
 export default Navbar;
