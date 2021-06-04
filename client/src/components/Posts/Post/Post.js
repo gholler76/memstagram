8bc6e9564079e83ba33ae5b9652f0ab626a9580f
Gary Holler <gholler76@gmail.com>
2021-06-04 13:57:43 -0400

    fixed formatting in likes and creator name

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -21,7 +21,7 @@ const Post = ( { post, setCurrentId } ) => {
                 ? (
                     <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${ post.likes.length - 1 } others` : `${ post.likes.length } like ${ post.likes.length > 1 ? 's' : '' }` }</>
                 ) : (
-                    <><ThumbUpAltOutlined fontSize="small" /> & nbsp; { post.likes.length } {post.likes.length === 1 ? 'Like' : 'Likes' }</>
+                    <><ThumbUpAltOutlined fontSize="small" /> &nbsp; { post.likes.length } {post.likes.length === 1 ? 'Like' : 'Likes' }</>
                 );
         }
         return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
@@ -34,11 +34,13 @@ const Post = ( { post, setCurrentId } ) => {
                 <Typography variant="h6">{ post.name }</Typography>
                 <Typography variant="body2">{ moment( post.createdAt ).fromNow() }</Typography>
             </div>
-            <div className={ classes.overlay2 }>
-                <Button style={ { color: "white" } } size="small" onClick={ () => setCurrentId( post._id ) }>
-                    <MoreHorizIcon fontSize="default" />
-                </Button>
-            </div>
+            { ( user?.result?.googleId === post?.creator || user?.result?._id === post?.creator ) && (
+                <div className={ classes.overlay2 }>
+                    <Button style={ { color: "white" } } size="small" onClick={ () => setCurrentId( post._id ) }>
+                        <MoreHorizIcon fontSize="default" />
+                    </Button>
+                </div>
+            ) }
             <Typography variant="body2" color="textSecondary">
                 { post.tags.map( ( tag ) => `#${ tag }` ) }
             </Typography>
@@ -54,12 +56,12 @@ const Post = ( { post, setCurrentId } ) => {
                 <Button size="small" color="primary" disabled={ !user?.result } onClick={ () => dispatch( likePost( post._id ) ) }>
                     <Likes />
                 </Button>
-            </CardActions>
-            <CardActions className={ classes.cardActions }>
-                <Button size="small" color="primary" onClick={ () => dispatch( deletePost( post._id ) ) }>
-                    <DeleteIcon fontSize="small" />
+                { ( user?.result?.googleId === post?.creator || user?.result?._id === post?.creator ) && (
+                    <Button size="small" color="primary" onClick={ () => dispatch( deletePost( post._id ) ) }>
+                        <DeleteIcon fontSize="small" />
                     Delete
-                </Button>
+                    </Button>
+                ) }
             </CardActions>
         </Card >
     );
